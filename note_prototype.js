b7f75b1b951ab9f254ecb61bebaa1fd129fcd199
Jonathan Rajavuori <jrajav@gmail.com>
2012-11-08 16:50:43 -0600

    Tests, note_prototype

---
+++ note_prototype.js
@@ -0,0 +1,14 @@
+var crypto = require('crypto');
+
+var note_prototype = {};
+
+note_prototype.title = null;
+note_prototype.contents = null;
+note_prototype.toString = function () {
+	var md5 = crypto.createHash('md5');
+	md5.update(this.title);
+	md5.update(this.contents);
+	return md5.digest();
+}
+
+module.exports = exports = note_prototype;
